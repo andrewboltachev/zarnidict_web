7b2636aa3eb6006a3a60826d13bf457347f8e613
andrewboltachev <andrewboltachev@gmail.com>
2016-05-13 18:25:05 +0300

    mmi

--- dicts/models.py
+++ dicts/models.py
@@ -1,8 +1,12 @@
 from django.db import models
+from django.conf import settings
 
 
 class Dictionary(models.Model):
     name = models.CharField(max_length=1024)
+    complete = models.BooleanField(default=False)
+    structure = models.CharField(max_length=256)
+    date_added = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.name
@@ -10,8 +14,26 @@ class Dictionary(models.Model):
 
 class Article(models.Model):
     dictionary = models.ForeignKey(Dictionary)
-    name = models.CharField(max_length=1024)
+    name = models.CharField(max_length=256)
+    
+    def __str__(self):
+        return '{1} ({0})'.format(self.dictionary, self.name)  # TODO named params # TODO gettext
+
+
+class NullUser(object):
+    username = '(No user)'
+
+
+class ArticleVersion(models.Model):
+    article = models.ForeignKey(Article)
     body = models.TextField(blank=True)
+    date_added = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, blank=True, null=True)
+    ACTION_CHOICES = [
+        (True, 'Add'),
+        (False, 'Retract'),
+    ]
+    action = models.BooleanField(default=True, choices=ACTION_CHOICES)
 
     def __str__(self):
-        return self.name
+        return '{article} by {user} at {datetime}'.format(user=(self.user or NullUser).username, article=self.article.name, datetime=self.date_added)
--- dicts/views.py
+++ dicts/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views.generics import View
+from django.views.decorators.csrf import csrf_exempt
 
-# Create your views here.
+
+class APIView(View):
+    @csrf_exempt
+    def dispatch(self, *args, **kwargs):
+        return super().dispatch(*args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        return HttpResponse('Hello world')
